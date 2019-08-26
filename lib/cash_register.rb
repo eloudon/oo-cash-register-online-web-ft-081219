2aafa97638c3c384b22f841fd41bcd108d7eaf2d
Eric Loudon <loudon.eric@gmail.com>
2019-08-26 08:08:12 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,42 +1,43 @@
 class CashRegister
 
-  attr_accessor :total, :discount, :last_transaction_amount, :items
+    attr_accessor :total, :discount, :cart, :last_transaction
 
-  def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-    @items = []
-  end
-
-
-  def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title
-        i+=1
-      end
-    else
-      @items << title
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @cart = []
+        @last_transaction = 0
     end
-    @total += price*quantity
-    @last_transaction_amount = @total
-    @total
-  end
 
-  def apply_discount()
-    if @discount > 0
-      @discount = @discount/100.to_f
-      @total = @total - (@total * (@discount))
-      "After the discount, the total comes to $#{@total.to_i}."
-    else
-      "There is no discount to apply."
+    def add_item(title, price, quantity = 1)
+        quantity.times do
+            @cart << title
+        end
+        # counter = 0
+        # while counter < quantity
+        #     @cart << title
+        #     counter += 1
+        # end
+        self.last_transaction = price * quantity
+        @total += self.last_transaction
     end
-  end
 
+    def apply_discount
+        #   binding.pry
+        if self.discount == 0
+            "There is no discount to apply."
+        else
+            @total = self.total * (1-(self.discount/100.0))
+            "After the discount, the total comes to $#{@total.to_i}."
+        end
+    end
 
-  def void_last_transaction()
-    @total -= @last_transaction_amount
-  end
+    def items
+        #return an array of all items that have been added.
+        @cart
+    end
 
+    def void_last_transaction
+        self.total -= self.last_transaction
+    end
 end
